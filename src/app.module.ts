8306f27311894cc9963926772125172b84f25f6b
robavelii <robelfekadu@gmail.com>
2023-08-03 23:11:53 +0300

    fix pre-commit lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Module } from '@nestjs/common';
 import * as path from 'path';
 import { MailerModule } from '@nestjs-modules/mailer';
